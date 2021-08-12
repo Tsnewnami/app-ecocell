6cd8e0f649f35ab8b3e5f7909b70ca55f6c55442
James Snewin <jamessnewin0@gmail.com>
2021-08-12 22:04:32 +1000

    added initial homepage components, material and angularflex imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app.routing.module';
 import { MaterialModule } from './material.module';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { HomepageComponent } from './components/homepage/homepage.component';
+import { HeaderComponent } from './components/homepage/header/header.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './components/homepage/login/login.component';
+import { SignupComponent } from './components/homepage/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    HeaderComponent,
+    LoginComponent,
+    SignupComponent,
   ],
   imports: [
     BrowserModule,
@@ -17,6 +26,8 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     MaterialModule,
     AppRoutingModule,
     FlexLayoutModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { HomepageComponent } from "./components/homepage/homepage.component";
 
 
 const routes: Routes = [
-  // { path: '', component: WelcomeComponent},
+  { path: '', component: HomepageComponent},
 
 ];
 
