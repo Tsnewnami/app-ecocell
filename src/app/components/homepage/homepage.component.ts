93f653657ff9bb5037f18595e458110b26f34db8
James Snewin <jamessnewin0@gmail.com>
2021-08-14 13:30:56 +1000

    added login and signup form

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
+  loginPage = true;
 
   constructor() { }
 
