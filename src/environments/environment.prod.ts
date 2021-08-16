c2163235b4e7f38b150cc9fa7818a3a1cb5221c9
James Snewin <jamessnewin0@gmail.com>
2021-08-16 16:18:22 +1000

    added firebase credentials to production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDiA3LROOuEYVqF13dJNuLzBulxyy8d5Dc",
+    authDomain: "app-ecocell.firebaseapp.com",
+    projectId: "app-ecocell",
+    storageBucket: "app-ecocell.appspot.com",
+    messagingSenderId: "250316474049",
+    appId: "1:250316474049:web:f584896d16028117d0b640",
+    measurementId: "G-VDJF7X2FS1"
+  }
 };
