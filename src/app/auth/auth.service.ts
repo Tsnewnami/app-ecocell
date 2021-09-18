fee4853940558d07a3c3ca4b4fb3417b70d1620b
James Snewin <jamessnewin0@gmail.com>
2021-09-18 12:39:18 +1000

    fixed error handling with login requests

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -28,14 +28,14 @@ export class AuthService {
     }
 
   async login(email: string, password: string){
-    this.isLoading.next(true);
+
     this.auth.setPersistence(firebase.auth.Auth.Persistence.SESSION).then(
       () =>
       this.auth.signInWithEmailAndPassword(
         email,
         password
       ).then(result => {
-
+        this.isLoading.next(true);
         const userAction: User = {id: result.user.uid, email: result.user.email}
 
         if(!result.user.emailVerified)
@@ -49,7 +49,7 @@ export class AuthService {
         this.router.navigate(['/app']);
       })
       .catch(err => {
-        this.snackBar.open(err.message, null, {duration :6000});
+        this.snackBar.open(this.parseError(err.code), null, {duration :6000});
       })
     );
   }
@@ -66,7 +66,7 @@ export class AuthService {
       })
     })
     .catch(err => {
-      this.snackBar.open(err.message, null, {duration :6000});
+      this.snackBar.open(this.parseError(err.code), null, {duration :6000});
     })
   }
 
@@ -86,6 +86,55 @@ export class AuthService {
     return this.auth.sendPasswordResetEmail(
       email,
       { url: 'https://app-ecocell.web.app/' });
+  }
+
+  parseError(errorCode: string): string {
+
+    let message: string;
+
+    switch (errorCode) {
+      case 'auth/wrong-password':
+        message = 'Invalid login credentials.';
+        break;
+      case 'auth/network-request-failed':
+        message = 'Please check your internet connection';
+        break;
+      case 'auth/too-many-requests':
+        message =
+          'We have detected too many requests from your device. Take a break please!';
+        break;
+      case 'auth/user-disabled':
+        message =
+          'Your account has been disabled or deleted. Please contact the system administrator.';
+        break;
+      case 'auth/requires-recent-login':
+        message = 'Please login again and try again!';
+        break;
+      case 'auth/email-already-exists':
+        message = 'Email address is already in use by an existing user.';
+        break;
+      case 'auth/user-not-found':
+        message =
+          'We could not find user account associated with the email address or phone number.';
+        break;
+      case 'auth/phone-number-already-exists':
+        message = 'The phone number is already in use by an existing user.';
+        break;
+      case 'auth/invalid-phone-number':
+        message = 'The phone number is not a valid phone number!';
+        break;
+      case 'auth/invalid-email  ':
+        message = 'The email address is not a valid email address!';
+        break;
+      case 'auth/cannot-delete-own-user-account':
+        message = 'You cannot delete your own user account.';
+        break;
+       default:
+        message = 'Oops! Something went wrong. Try again later.';
+        break;
     }
+
+    return message;
+  }
 
 }
